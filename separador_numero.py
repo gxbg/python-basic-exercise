b1bda465a94ff550c729dff820fe4e0ee44da32c
gxbg <gabsgil0105@gmail.com>
2026-03-05 18:09:49 -0300

    Update separador_numero.py

--- separador_numero.py
+++ separador_numero.py
@@ -1,1 +1,13 @@
+numero = input('Digite um número entre 0 e 999: ') #Input pro usuário escolher seu número
+numero = int(numero) #Converte a string pra número inteiro
 
+centena = (numero//100) * 100 #Separa a centena, o comando "//" divide um número pelo outro retornando apenas a parte inteira da divisão
+dezena = (((numero % 100) // 10) * 10) #Separa a dezena, o comando "%" divide um número pelo outro retornando só o resto da divisão
+unidade = (numero % 100) % 10 #Separa a unidade
+
+#Output que vai mostrar a separação do número
+
+print('Seu número é:', numero)
+print('A centena dele é:', centena)
+print('A dezena dele é:', dezena)
+print('A unidade dele é:', unidade)
