12de3268ca33aaaab8f4e93d78f9ff35703ae4fb
gxbg <gabsgil0105@gmail.com>
2026-03-05 18:11:02 -0300

    Update dias_em_idade.py

--- dias_em_idade.py
+++ dias_em_idade.py
@@ -1,1 +1,8 @@
+total_dias = input('Digite a idade em dias: ')
+total_dias = int(total_dias)
 
+anos = total_dias // 365
+meses = (total_dias % 365) // 30
+dias = (total_dias % 365) % 30
+
+print('\n Você tem', anos, 'anos,', meses, 'meses e', dias, 'dias!')
